a8c0171f0359e4c520febbdc3e4cfa5ac98416fb
deekshita Madhalam <madhalam.d@northeastern.edu>
2024-12-04 15:35:08 -0500

    Update reviews_routes.py

--- api/backend/reviews/reviews_routes.py
+++ api/backend/reviews/reviews_routes.py
@@ -11,7 +11,7 @@ from backend.db_connection import db
 reviews = Blueprint('reviews', __name__)
 
 # ------------------------------------------------------------
-# Return all reviews written
+# Return all reviews
 @reviews.route('/reviews', methods = ['GET'])
 def get_reviews():
     query = '''
@@ -50,7 +50,7 @@ def get_user_reviews(user_id):
     return response
 
 # ------------------------------------------------------------
-# Add an anonymous review to the database
+# Add an anonymous review
 @reviews.route('/reviews/<user_id>', methods = ['POST'])
 def add_user_reviews(user_id):
     the_data = request.json
@@ -63,21 +63,21 @@ def add_user_reviews(user_id):
     cursor = db.get_db().cursor()
     cursor.execute(query)
     db.get_db().commit()
- 
+
     response = make_response("Review added successfully")
     response.status_code = 200
     return response
 
 # ------------------------------------------------------------
-# Update a review previously written by a student
-@reviews.route('/reviews/<user_id>/<review_id>', methods = ['PUT'])
-def update_user_reviews(user_id, review_id):
+# Update a review
+@reviews.route('/reviews/<user_id>/,<review_id>', methods = ['PUT'])
+def update_user_reviews(user_id):
     the_data = request.json
     query = f'''
         UPDATE Review
         SET summary = '{the_data["summary"]}', bestPart = '{the_data["bestPart"]}', 
             worstPart = '{the_data["worstPart"]}', rating = {the_data["rating"]}
-        WHERE reviewId = '{review_id}' AND createdBy = '{user_id}'
+        WHERE reviewId = {the_data["reviewId"]} AND createdBy = '{user_id}'
     '''
     cursor = db.get_db().cursor()
     cursor.execute(query)
@@ -126,13 +126,13 @@ def get_company_reviews(company_id):
 
 # ------------------------------------------------------------
 # Return reviews for a specific role
-@reviews.route('/reviews/<company_id>/<role_id>', methods = ['GET'])
-def get_role_reviews(company_id, role_id):
+@reviews.route('/reviews/<company_id>/<job_id>', methods = ['GET'])
+def get_role_reviews(company_id, job_id):
     query = f'''
         SELECT reviewId, createdAt, salary, rating, summary, 
                bestPart, worstPart, isAnonymous
         FROM Review
-        WHERE role = '{role_id}' AND companyId = '{company_id}'
+        WHERE role = '{job_id}' AND companyId = '{company_id}'
         ORDER BY createdAt DESC
     '''
 
@@ -144,14 +144,14 @@ def get_role_reviews(company_id, role_id):
     response.status_code = 200
     return response
 
- 
+
 # ------------------------------------------------------------
 # Returns reviews for other companies to compare it to my company
 @reviews.route('/reviews/compare/<company_id>', methods = ['GET'])
 def get_company_comparisons(company_id):
     query = '''
-        
-    ''' 
+       
+    '''
 
     cursor = db.get_db().cursor()
     cursor.execute(query)

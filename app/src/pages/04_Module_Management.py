211ea0d0c1ec78f837c1dac5f93d43b9327838c0
tsudhakar87 <sudhakar.th@northeastern.edu>
2024-12-04 20:28:25 -0500

    Attempt at new module page
    trying something, will continue updating

---
+++ app/src/pages/04_Module_Management.py
@@ -0,0 +1,17 @@
+import logging
+
+import requests
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout = 'wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Manage Modules")
+
+results = requests.get('http://api:4000/backend/10/25').json()
+

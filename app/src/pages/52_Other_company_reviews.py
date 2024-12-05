fce8b44ac6ce75228783860146aa53782cab8d5c
deekshita Madhalam <madhalam.d@northeastern.edu>
2024-12-05 12:51:42 -0500

    trial

---
+++ app/src/pages/52_Other_company_reviews.py
@@ -0,0 +1,12 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+import requests
+
+st.set_page_config(layout = 'wide')
+
+SideBarLinks()  
+
+st.title('Compare with Other Companies')

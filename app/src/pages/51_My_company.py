3ba414d0185ec5e265e2d47ada78d9ca27d9bf7c
deekshita Madhalam <madhalam.d@northeastern.edu>
2024-12-05 13:08:19 -0500

    Update 51_My_company.py

--- app/src/pages/51_My_company.py
+++ app/src/pages/51_My_company.py
@@ -14,9 +14,9 @@ st.title('My Companies Reviews')
 
 col1, col2, col3 = st.columns(3)
 
-with col1:\
-    role = st.text_input('Filter by Role (Job ID)', placeholder='e.g., 45')
+with col1:
     rating_min = st.number_input('Minimum Rating', min_value=0.0, max_value=5.0, step=0.1)
+    role = st.text_input('Filter by Role (Job ID)', placeholder='e.g., 45')
 
 with col2:
     date_from = st.date_input('Start Date')

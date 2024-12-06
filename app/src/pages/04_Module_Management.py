0c3dc1c85fe35196f6515e242cdfe52d0e6be97c
tsudhakar87 <sudhakar.th@northeastern.edu>
2024-12-05 23:53:09 -0500

    moved [ to better location
    previously the [ was in a different location that was not optimal. In order to maximize the efficacy of our efforts. I took initiative and decided enough was enough. The [ simply had to be moved. It was a hefty task. But I did it anyways. because. that is the kind of person i am. it aligns with my core values of nu-clear-ing. <3

--- app/src/pages/04_Module_Management.py
+++ app/src/pages/04_Module_Management.py
@@ -25,7 +25,7 @@ st.popover("Assign a new module", help=None, icon=None, disabled=False, use_cont
 
 ## dummy dataframe view for now bc endpoint for modules hasn't been tested yet
 df = pd.DataFrame(
-    [
+    [ 
         {"command": "st.selectbox", "rating": 4, "is_widget": True},
         {"command": "st.balloons", "rating": 5, "is_widget": False},
         {"command": "st.time_input", "rating": 3, "is_widget": True},

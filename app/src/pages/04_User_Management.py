ce608976871385953b1db2be812d82ecdd431215
tsudhakar87 <sudhakar.th@northeastern.edu>
2024-12-06 15:31:18 -0500

    Updated add user perm -> add user instead
    makes more sense since we want to limit to 5 permissions

--- app/src/pages/04_User_Management.py
+++ app/src/pages/04_User_Management.py
@@ -23,46 +23,12 @@ def clear_text():
   st.session_state["text4"] = ''
 
 # popover to add a new permission type
-with st.popover("Add a new permission type", help=None, icon=None, disabled=False, use_container_width=True):
-    createdBy = st.text_input("Admin Id:", key="text1")
+with st.popover("Add a new user", help=None, icon=None, disabled=False, use_container_width=True):
     userType = st.text_input("User Type:", key="text2")
-    
-    ## create perms
-    canCreateReview = st.selectbox('Able to create a review?', (0, 1))
-    canCreateCoopListing = st.selectbox('Able to create a co-op listing?', (0, 1))
-    canCreateModule = st.selectbox('Able to create a module?', (0, 1))
-    
-    ## edit perms
-    canEditPerms = st.selectbox('Able to edit permissions?', (0, 1))
-    canEditModule = st.selectbox('Able to edit modules?', (0, 1))
-    canEditAccSettings = st.selectbox('Able to edit account settings?', (0, 1))
-   
-    ## view perms
-    canViewReview = st.selectbox('Able to view a review?', (0, 1))
-    canViewCoopListing = st.selectbox('Able to view a co-op listing?', (0, 1))
-    canViewModule = st.selectbox('Able to view a module?', (0, 1))
-    
-    ## delete perms
-    canDeleteReview = st.selectbox('Able to delete a review?', (0, 1))
-    canDeleteCoopListing = st.selectbox('Able to delete a co-op listing?', (0, 1))
-    canDeleteModule = st.selectbox('Able to delete a module?', (0, 1))
     
     if st.button("Add", on_click=clear_text):
         filters = {
-            "createdBy": createdBy,
             "userType": userType,
-            "canEditPerms": canEditPerms,
-            "canEditModule": canEditModule,
-            "canEditAccSettings": canEditAccSettings,
-            "canCreateReivew": canCreateReview,
-            "canCreateCoopListing": canCreateCoopListing,
-            "canCreateModule": canCreateModule,
-            "canViewReview": canViewReview,
-            "canViewCoopListing": canViewCoopListing,
-            "canViewModule": canViewModule,
-            "canDeleteReview": canDeleteReview,
-            "canDeleteCoopListing": canDeleteCoopListing,
-            "canDeleteModule": canDeleteModule
         }
         requests.post(url, json=filters)
 
